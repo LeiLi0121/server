a5c2182a6980ac219c2b53447b070e84219b84d6
LeiLi0121 <128519250+LeiLi0121@users.noreply.github.com>
2023-11-29 16:26:09 +0000

    Update broker.go

--- broker/broker.go
+++ broker/broker.go
@@ -104,7 +104,6 @@ type GolOp struct {
 	pause         chan bool
 	needResume    bool
 	needSave      chan bool
-	//needKill      chan bool
 	//worldBeforeQuit [][]uint8
 	//turnBeforeQuit  int
 
@@ -128,6 +127,7 @@ func (g *GolOp) ExecuteTurns(req gol.Request, res *gol.Response) (err error) {
 		res.Final = gol.FinalTurnComplete{CompletedTurns: req.P.Turns, Alive: calculateAliveCells(g.world)}
 		return
 	} else {
+		//workers := []string{"54.227.67.232:8030", "3.90.111.104:8030", "54.237.222.162:8030", "3.87.240.120:8030"}
 		workers := []string{"35.172.226.22:8030", "54.84.139.55:8030", "54.82.127.203:8030", "52.91.247.6:8030"}
 		//workers := []string{"127.0.0.1:8031", "127.0.0.1:8032", "127.0.0.1:8033", "127.0.0.1:8034"}
 		clients := make([]*rpc.Client, len(workers))
@@ -136,71 +136,7 @@ func (g *GolOp) ExecuteTurns(req gol.Request, res *gol.Response) (err error) {
 			defer client.Close()
 			clients[i] = client
 		}
-		partHeight := req.P.ImageHeight / len(clients)
-		haloWorker := make([]worker, 4)
-		worker1 := worker{
-			worldPart: initWorld(partHeight, req.P.ImageWidth),
-			upperHalo: make(chan []uint8, 1),
-			lowerHalo: make(chan []uint8, 1),
-		}
-		//fmt.Println("here", 1)
-		worker1.upperHalo <- g.world[req.P.ImageHeight-1]
-		//copy(worker1.upperHalo, g.world[req.P.ImageHeight-1])
-		//fmt.Println("here", 2)
-		worker1.lowerHalo <- g.world[partHeight]
-		//copy(worker1.lowerHalo, g.world[req.P.ImageHeight])
-		for i := 0; i < partHeight; i++ {
-			copy(worker1.worldPart[i], g.world[i])
-		}
-		haloWorker[0] = worker1
-		worker2 := worker{
-			worldPart: initWorld(partHeight, req.P.ImageWidth),
-			upperHalo: make(chan []uint8, 1),
-			lowerHalo: make(chan []uint8, 1),
-		}
-		worker2.upperHalo <- g.world[partHeight-1]
-		//fmt.Println("here", 3)
-		//copy(worker2.upperHalo, g.world[partHeight-1])
-		worker2.lowerHalo <- g.world[2*partHeight]
-		//fmt.Println("here", 4)
-		//copy(worker2.lowerHalo, g.world[2*partHeight])
-		for i := partHeight; i < 2*partHeight; i++ {
-			copy(worker2.worldPart[i-partHeight], g.world[i])
-		}
-		haloWorker[1] = worker2
-		worker3 := worker{
-			worldPart: initWorld(partHeight, req.P.ImageWidth),
-			upperHalo: make(chan []uint8, 1),
-			lowerHalo: make(chan []uint8, 1),
-		}
-		worker3.upperHalo <- g.world[2*partHeight-1]
-		//fmt.Println("here", 5)
-		//copy(worker3.upperHalo, g.world[2*partHeight-1])
-		worker3.lowerHalo <- g.world[3*partHeight]
-		//fmt.Println("here", 6)
-		//copy(worker3.lowerHalo, g.world[3*partHeight])
-		for i := 2 * partHeight; i < 3*partHeight; i++ {
-			copy(worker3.worldPart[i-2*partHeight], g.world[i])
-		}
-		haloWorker[2] = worker3
-		worker4 := worker{
-			worldPart: initWorld(partHeight, req.P.ImageWidth),
-			upperHalo: make(chan []uint8, 1),
-			lowerHalo: make(chan []uint8, 1),
-		}
-		worker4.upperHalo <- g.world[3*partHeight-1]
-		//fmt.Println("here", 7)
-		//copy(worker4.upperHalo, g.world[3*partHeight-1])
-		worker4.lowerHalo <- g.world[0]
-		//fmt.Println("here", 8)
-		//copy(worker4.lowerHalo, g.world[0])
-		for i := 3 * partHeight; i < 4*partHeight; i++ {
-			copy(worker4.worldPart[i-3*partHeight], g.world[i])
-		}
-		haloWorker[3] = worker4
-		//fmt.Println("here", 9)
 		for t := 0; t < req.P.Turns; t++ { //t need to be turn when extention mode
-			fmt.Println("turn", t)
 			select {
 			case <-g.pause:
 				pause := <-g.pause
@@ -212,59 +148,21 @@ func (g *GolOp) ExecuteTurns(req gol.Request, res *gol.Response) (err error) {
 				return
 			default:
 				break
-
 			}
 			allPartsChan := make(chan [][]uint8, 1)
-			if t == 1 {
-				worker1.worldPart = nil
-				worker2.worldPart = nil
-				worker3.worldPart = nil
-				worker4.worldPart = nil
-			}
-
+			partHeight := req.P.ImageHeight / len(clients)
+			//worker1:=worker{
+			//	worldPart: make([][]uint8, partHeight+2),
+			//}
 			for i := 0; i < len(clients); i++ {
-				//fmt.Println("in client", i)
-				//go func(i int) {
-				//	partInfo := gol.PartInfo{g.world, i * partHeight, (i + 1) * partHeight, req.P.ImageWidth}
-				//	newPart := new(gol.NewPart)
-				//	clients[i].Call(gol.WorkerProcess, partInfo, newPart)
-				//	mutex.Lock()
-				//	for h := i * partHeight; h < (i+1)*partHeight; h++ {
-				//		copy(res.NewWorld[h], newPart.NewWorldPart[h])
-				//	}
-				//	allPartsChan <- res.NewWorld
-				//	mutex.Unlock()
-				//}(i)
+
 				go func(i int) {
-					//request := gol.HaloReq{haloWorker[i].worldPart, <-haloWorker[i].upperHalo,
-					//	<-haloWorker[i].lowerHalo, partHeight, req.P.ImageWidth}
-					request := gol.HaloReq{nil, <-haloWorker[i].upperHalo,
-						<-haloWorker[i].lowerHalo, partHeight, req.P.ImageWidth}
-					if t == 0 {
-						request.WorldPart = haloWorker[i].worldPart
-					}
-					response := new(gol.HaloRes)
-					err := clients[i].Call(gol.WorkerProcess, request, response)
-					if err != nil {
-						return
-					}
+					partInfo := gol.PartInfo{g.world, i * partHeight, (i + 1) * partHeight, req.P.ImageWidth}
+					newPart := new(gol.NewPart)
+					clients[i].Call(gol.WorkerProcess, partInfo, newPart)
 					mutex.Lock()
 					for h := i * partHeight; h < (i+1)*partHeight; h++ {
-						copy(res.NewWorld[h], response.WorldPart[h-i*partHeight])
-					}
-					if i == 0 { //1
-						worker2.upperHalo <- response.WorldPart[partHeight-1]
-						worker4.lowerHalo <- response.WorldPart[0]
-					} else if i == 1 { //2
-						worker1.lowerHalo <- response.WorldPart[0]
-						worker3.upperHalo <- response.WorldPart[partHeight-1]
-					} else if i == 2 {
-						worker2.lowerHalo <- response.WorldPart[0]
-						worker4.upperHalo <- response.WorldPart[partHeight-1]
-
-					} else {
-						worker1.upperHalo <- response.WorldPart[partHeight-1]
-						worker3.lowerHalo <- response.WorldPart[0]
+						copy(res.NewWorld[h], newPart.NewWorldPart[h])
 					}
 					allPartsChan <- res.NewWorld
 					mutex.Unlock()
@@ -285,7 +183,7 @@ func (g *GolOp) ExecuteTurns(req gol.Request, res *gol.Response) (err error) {
 		}
 
 		res.Final = gol.FinalTurnComplete{CompletedTurns: req.P.Turns, Alive: calculateAliveCells(res.NewWorld)}
-		//fmt.Println("game/test finished")
+		fmt.Println("game/test finished")
 		return
 	}
 
@@ -335,13 +233,7 @@ func (g *GolOp) KeyOp(op gol.KeyPress, res *gol.Response) (err error) {
 	return
 }
 
-//func (g *GolOp) Kill(op gol.KeyPress, res *gol.Response) (err error) {
-//	os.Exit(0)
-//	return
-//}
-
 func (g *GolOp) Kill(op gol.KeyPress, res *gol.Response) (err error) {
-	//g.needKill <- true
 	os.Exit(0)
 	return
 }
@@ -385,6 +277,6 @@ func initWorld(height, width int) [][]uint8 {
 
 type worker struct {
 	worldPart [][]uint8
-	upperHalo chan []uint8
-	lowerHalo chan []uint8
+	upperHalo []uint8
+	lowerHalo []uint8
 }
